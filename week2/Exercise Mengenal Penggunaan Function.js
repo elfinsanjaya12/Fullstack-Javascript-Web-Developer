8e0f2a08741b04318eb8934139981bc07386abe0
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-21 17:06:48 +0700

    Exercise Mengenal Penggunaan Function

---
+++ week2/Exercise Mengenal Penggunaan Function.js
@@ -0,0 +1,32 @@
+// //Tugas 1
+// function shoutOut(){
+//     return 'Halo Function!';
+// }
+// console.log(shoutOut());
+
+// // Tugas 2
+// function calculateMultiyly(num1, num2){
+//     return num1 * num2
+// }
+// var num1 = 5;
+// var num2 = 6;
+
+// var hasilPerkalian = calculateMultiyly(num1, num2);
+// console.log(hasilPerkalian);
+
+// Tugas 3
+
+function prosessSentence(name, age, address, hobby){
+    return 'Nama saya ' + name + ' umur saya ' + age + ', alamat saya di ' + address + ', dan saya punya yaitu ' + hobby + '!'
+}
+
+var name    = "Agus";
+var age     = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby   = "gaming";
+
+var fullSentence = prosessSentence(name, age, address, hobby);
+console.log(fullSentence);
+
+
+
