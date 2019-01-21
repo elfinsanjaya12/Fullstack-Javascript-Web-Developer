4c690d14f45d95f54455b5c14683c5079647bef9
SenimanMalam001 <elfinsanjaya12@gmail.com>
2019-01-21 17:59:24 +0700

    case 9:

---
+++ week2/Exercise Mengenal penggunaan Conditional Switch .js
@@ -0,0 +1,57 @@
+var hari    = 1
+var bulan   = 1
+var tahun   = 1945
+
+switch(bulan){
+    case 1:
+        bulan = 'Januari'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 2:
+        bulan = 'Febuari'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 3:
+        bulan = 'Maret'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 4:
+        bulan = 'April'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 5:
+        bulan = 'Mei'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 6:
+        bulan = 'Juni'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 7:
+        bulan = 'Juli'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 8:
+        bulan = 'Agustus'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 9:
+        bulan = 'September'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 10:
+        bulan = 'Oktober'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 11:
+        bulan = 'November'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    case 12:
+        bulan = 'Desember'
+        console.log(hari + ' ' + bulan + ' ' + tahun)
+        break;
+    default :
+        console.log('Bulan nya gak ada bro')
+}
+
