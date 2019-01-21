dc0dc8eb22233d47faf827f2bc2bef0c5c665a1c
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-21 16:39:21 +0700

    Exercise Mampu menggabungkan konsep Conditional dan Looping dalam kasus sederhana

---
+++ week2/Exercise Mampu menggabungkan konsep Conditional dan Looping dalam kasus sederhana.js
@@ -0,0 +1,32 @@
+// for(var counter = 1; counter <= 100; counter++){
+//     if(counter % 2 === 0){
+//         console.log("contoh - ganjil genap");
+//         console.log("counter sekarang = " + counter);
+//         console.log("GENAP")
+//     }else {
+//         console.log("contoh - ganjil genap");
+//         console.log("counter sekarang = " + counter);
+//         console.log("GANJIL")
+//     }
+// }
+
+// for(var counter = 1; counter <= 100; counter+=2){
+//     if(counter % 3 ===0){
+//         console.log("counter sekarang = " + counter)
+//         console.log(counter + ' KELIPAN 3')
+//     }    
+// }
+
+// for(var counter = 1; counter <= 100; counter+=5){
+//     if(counter % 6 ===0){
+//         console.log("counter sekarang = " + counter)
+//         console.log(counter + ' KELIPAN 6')
+//     }    
+// }
+
+for(var counter = 1; counter <= 100; counter+=9){
+    if(counter % 10 ===0){
+        console.log("counter sekarang = " + counter)
+        console.log(counter + ' KELIPAN 10')
+    }    
+}
