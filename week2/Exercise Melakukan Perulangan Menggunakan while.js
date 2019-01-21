b927648bb249d2ee2e53ca41ac9f13c3efb3c9d5
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-21 15:58:22 +0700

    Exercise Melakukan Perulangan Menggunakan while

---
+++ week2/Exercise Melakukan Perulangan Menggunakan while.js
@@ -0,0 +1,13 @@
+var i = 2
+console.log('LOOPING PERTAMA')
+while(i <= 20){
+    console.log(i + ' - I love coding')
+    i+=2
+}
+
+var x = 20
+console.log('LOOPING KEDUA')
+while(x >= 2){
+    console.log(x + ' - I will become fullstack developer')
+    x-=2
+}
