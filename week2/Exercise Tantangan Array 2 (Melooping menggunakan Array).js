03b5a9f1a797679b47e5f3075a4d708ddfddae48
SenimanMalam001 <elfinsanjaya12@gmail.com>
2019-01-22 11:19:01 +0700

    Exercise Tantangan Array 2 (Melooping menggunakan Array)

---
+++ week2/Exercise Tantangan Array 2 (Melooping menggunakan Array).js
@@ -0,0 +1,18 @@
+function dataHandaling(){
+
+    var input = [
+        ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+        ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+        ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+        ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+    ]
+    for(var i = 0; i < input.length; i++){
+        console.log("Nomor ID: " + input[i][0])
+        console.log("Nama Lengkap: " + input[i][1])
+        console.log("TTL: " + input[i][2] + " " + input[i][3])
+        console.log("Nomor Hobi: " + input[i][4])
+    }
+}
+
+console.log(dataHandaling());
+
